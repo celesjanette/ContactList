d9a57bd6ac9158a0b515e4a816f7b5b34a9c7db3
celesjanette <celesaug1@gmail.com>
2024-02-03 22:24:14 -0500

    Added new java class ContactAdapter, made updates to datasource, and started list layout

---
+++ app/src/main/java/com/example/contactlist/ContactAdapter.java
@@ -0,0 +1,48 @@
+package com.example.contactlist;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.view.View;
+import java.util.ArrayList;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ContactAdapter extends RecyclerView.Adapter {
+    private ArrayList<String> contactData;
+    public class ContactViewHolder extends RecyclerView.ViewHolder {
+        public TextView textViewContact;
+        public ContactViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewContact = itemView.findViewById(R.id.textViewName);
+        }
+
+        public TextView getContactTextView() {
+            return textViewContact;
+        }
+    }
+    public ContactAdapter(ArrayList<String> arrayList){
+        contactData = arrayList;
+    }
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+      View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.simple_item_view, parent, false);
+        return new ContactViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ContactViewHolder cvh = (ContactViewHolder) holder;
+        cvh.getContactTextView().setText(contactData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactData.size();
+    }
+
+}
--- app/src/main/java/com/example/contactlist/ContactDataSource.java
+++ app/src/main/java/com/example/contactlist/ContactDataSource.java
@@ -5,11 +5,13 @@ import android.database.sqlite.SQLiteDatabase;
 import android.content.Context;
 import android.content.ContentValues;
 import android.database.Cursor;
+import java.util.ArrayList;
 
 
 public class ContactDataSource {
     private SQLiteDatabase database;
     private ContactDBHelper dbHelper;
+
 
     public ContactDataSource(Context context) {
 
@@ -81,4 +83,21 @@ public class ContactDataSource {
         }
         return lastId;
     }
+public ArrayList<String> getContactNames() {
+        ArrayList<String> contactNames = new ArrayList<>();
+        try {
+            String query = "Select contactname from contact";
+            Cursor cursor = database.rawQuery(query, null);
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+                contactNames.add(cursor.getString(0));
+                cursor.moveToNext();
+        }
+    cursor.close();
+    }
+    catch (Exception e) {
+        contactNames = new ArrayList<>();
+    }
+    return contactNames;
+}
 }
